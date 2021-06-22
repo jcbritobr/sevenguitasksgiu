3c13cb0bc7a446519dc81b08c094c357af5519a2
jcbritobr <jcbritobr@gmail.com>
2021-06-22 20:04:44 -0300

    Fix redo undo.

--- cmd/circles/main.go
+++ cmd/circles/main.go
@@ -10,7 +10,21 @@ import (
 var (
 	pos        = image.Point{}
 	circleList = []image.Point{}
+	counter    = 0
+	index      = 0
 )
+
+func undo() {
+	if index > 0 {
+		index--
+	}
+}
+
+func redo() {
+	if index < counter {
+		index++
+	}
+}
 
 func loop() {
 	giu.SingleWindow("canvas").Layout(
@@ -19,14 +33,20 @@ func loop() {
 			pos = giu.GetMousePos()
 			c := color.RGBA{255, 0, 0, 255}
 
-			for _, position := range circleList {
+			for _, position := range circleList[:index] {
 				canvas.AddCircleFilled(position, 30, c)
 			}
 
-			if giu.IsMouseClicked(giu.MouseButtonLeft) {
+			if giu.IsMouseClicked(giu.MouseButtonLeft) && pos.Y > 70 {
 				circleList = append(circleList, pos)
+				counter++
+				index++
 			}
 		}),
+		giu.Row(
+			giu.Button("Undo").Size(100, 25).OnClick(undo),
+			giu.Button("Redo").Size(100, 25).OnClick(redo),
+		),
 	)
 }
 
